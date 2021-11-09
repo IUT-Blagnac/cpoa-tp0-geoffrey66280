448b2a15d0753feb9cb562476408afcd851dbc5c
geoffrey66280 <geoffrey160502@hmail.com>
2021-11-09 15:27:30 +0100

    modif classe order et StepDefinitions

--- src/test/java/Order.java
+++ src/test/java/Order.java
@@ -2,9 +2,35 @@ import java.util.List;
 import java.util.LinkedList;
 
 public class Order {
+	private String Owner;
+	private String Recipient;
+	
+	public Order() {
+		this.Owner = Owner;
+		this.Recipient = Recipient;
+	}
+	
+	public Order(String owner, String recipient) {
+		this.Owner = owner;
+		this.Recipient = recipient;
+		
+	}
 
-    public void setOwner(String who) { /* ...*/ }
-    public void setRecipient(String who) { /* ...*/ }
+    public void setOwner(String who) {
+    this.Owner = Owner;
+    }
+    public void setRecipient(String who) {
+    this.Recipient = Recipient;
+    }
+    
+    public String getOwner() {
+    	return this.Owner;
+    }
+    
+    public String getRecipient() {
+    	return this.Recipient;
+    }
+    
     public List<Order.Drink> getDrinks() { return new LinkedList<>(); }
 
     static class Drink {
---
+++ src/test/java/StepDefinitions.java
@@ -0,0 +1,24 @@
+import io.cucumber.java.en.*;
+import java.util.List;
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+    private Order o;
+
+    @Given("Romeo who wants to create an Order")
+    public void romeo_who_wants_to_create_an_order() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Juliet is declared as recipient")
+    public void juliet_is_declared_as_recipient() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the order does not contain any drinks")
+    public void the_order_does_not_contain_any_drinks() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
